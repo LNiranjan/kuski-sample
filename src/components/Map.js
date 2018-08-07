c2d5d63ea88b013852b927635b4193fcc0a527ec
priyakvana <kvanadev1@gmail.com>
2018-08-07 16:51:34 +0530

    date validation added

--- src/components/Map.js
+++ src/components/Map.js
@@ -31,7 +31,18 @@ class Map extends Component {
       book_time,
       book_hours,
     } = this.state;
-    if( book_date>=new Date()){
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth()+1;
+    var yyyy = today.getFullYear();
+    if(dd<10) {
+      dd = '0'+dd
+    } 
+    if(mm<10) {
+      mm = '0'+mm
+    } 
+    today = yyyy+'-'+mm+'-'+dd;
+    if( book_date >= today) {
       db.locationTracking(auth.currentUser().uid, this.props.lat, this.props.lng, book_date, book_time, book_hours)
       .then(
         console.log("current location stored.....")
